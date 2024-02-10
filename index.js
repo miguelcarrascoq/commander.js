d7116792c67507a6a61eac01ce93510f895193c7
Miguel Carrasco Q <miguel.carrasco.quijada@gmail.com>
2024-02-10 15:47:32 -0300

    add more options (get from an api & version)

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const { program } = require('commander');
+const axios = require('axios');
 
 program
+    .option('-v, --version', 'output the version number')
+    .option('-i, --indicadores', 'fetch data from mindicador.cl/api')
     .option('--first')
-    .option('-s, --separator <char>');
-
-program.parse();
+    .option('-s, --separator <char>', 'output the first path segment')
+    .parse(process.argv);
 
 const options = program.opts();
-const limit = options.first ? 1 : undefined;
-console.log(program.args[0].split(options.separator, limit));
+
+if ('version' in options) {
+    console.log('Version: 1.0.0');
+}
+
+if ('indicadores' in options) {
+    console.log('Fetching data from mindicador.cl/api')
+    axios.get('https://mindicador.cl/api')
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(`Error: ${error}`);
+        })
+}
+
+if ('first' in options) {
+    console.log(options.separator)
+    const limit = options.first ? 1 : undefined;
+    console.log(program.args[0].split(options.separator, limit));
+}
+
